e01a004456943e1ec29c37f7498395772f83405e
Sofiya112 <sofiya032005@gmail.com>
2025-04-24 11:12:21 +0300

    Add files via upload
    Задание 3. Используя Visual Studio, создайте проект по шаблону Console Application.
    
    1. Создайте статический метод double Average (int m, int n), который вычисляет и возвращает среднее арифметическое всех целых чисел от m до n включительно.
    
    2. Объявите тип делегата, соответствующий этому методу.
    
    3. Объявите сам делегат.
    
    4. Применить в программе созданный делегат 3 раза.

---
+++ Task_3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+
+        public delegate double Average(int m, int n);
+        public static double AverageD(int m, int n)
+        {
+            // Проверка на корректность значений
+            if (m > n)
+            {
+                Console.WriteLine("Ошибка: m должно быть меньше или равно n.");
+                return 0;
+            }
+
+            int sum = 0;
+            int count = 0;
+
+            // Вычисление суммы всех чисел от m до n включительно
+            for (int i = m; i <= n; i++)
+            {
+                sum += i;
+                count++;
+            }
+
+            // Возвращаем среднее арифметическое
+            return (double)sum / count;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Average del1 = new Average(AverageD);
+            Average del2 = new Average(AverageD);
+            Average del3 = new Average(AverageD);
+
+            // Применение делегатов с разными диапазонами
+            double result1 = del1.Invoke(1, 5);
+            Console.WriteLine("Среднее арифметическое от 1 до 5: " + result1);
+
+            double result2 = del2(10, 20);
+            Console.WriteLine("Среднее арифметическое от 10 до 20: " + result2);
+
+            double result3 = del3(30, 40);
+            Console.WriteLine("Среднее арифметическое от 30 до 40: " + result3);
+
+
+            Console.ReadLine();
+        }
+    }
+}
