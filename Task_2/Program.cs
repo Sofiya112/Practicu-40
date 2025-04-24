132caf4f0800e80a02980809ce2ba8b45767d9f7
Sofiya112 <sofiya032005@gmail.com>
2025-04-24 11:04:16 +0300

    Add files via upload
    Задание 2. Используя Visual Studio, создайте проект по шаблону Console Application.
    1. Создайте статические методы вычисления и вывода суммы, разности, произведения, деления.
    
    2. Объявите тип делегата, соответствующий этому методу.
    
    3. Создайте сам делегат.
    
    4. Применить в программе созданный делегат для вызова созданных методов.

---
+++ Task_2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        delegate int Operation(int x, int y);
+
+        static void Main(string[] args)
+        {
+
+            Operation del = new Operation(Add);
+            int result = del.Invoke(4, 5); 
+            Console.WriteLine("Сумма: " + result);
+
+            del = Subtraction;
+            result = del.Invoke(4, 5); 
+            Console.WriteLine("Разность: " + result);
+
+            del = Multiply;
+            result = del.Invoke(4, 5); 
+            Console.WriteLine("Произведение: " + result);
+
+            
+            del = Division;
+            result = del.Invoke(4, 2); 
+            Console.WriteLine("Деление: " + result);
+
+            Console.Read();
+
+        }
+        private static int Add(int x, int y)  // Сумма
+        {
+            return x + y;
+        }
+
+        private static int Subtraction(int x, int y)  // Разность
+        {
+            return x - y;
+        }
+
+        private static int Multiply(int x, int y)  // Произведение
+        {
+            return x * y;
+        }
+
+        private static int Division(int x, int y)  // Деление
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("Ошибка: деление на ноль.");
+                return 0;
+            }
+            return x / y;
+        }
+    }
+}
