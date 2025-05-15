24b1dcd0a8242db8fc35d6fc7ad50109e444dcfb
Sofiya112 <sofiya032005@gmail.com>
2025-05-15 10:39:02 +0300

    Add files via upload
    Задание 4. Используя Visual Studio, создайте проект по шаблону Console Application.
    
    1. Создайте статический метод int Sum (int m, int n), который вычисляет и возвращает сумму всех целых чисел от m до n включительно.
    
    2. Объявите тип делегата, соответствующий этому методу.
    
    3. Объявите сам делегат.
    
    4. Применить в программе созданный делегат 3 раза.

---
+++ Task 4 pr4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4_pr4
+{
+  
+    
+    class Program
+    {
+        //вычисления суммы чисел от m до n
+        static int SumRange(int m, int n)
+        {
+            int sum = 0;
+            for (int i = m; i <= n; i++)
+                sum += i;
+            return sum; 
+        }
+
+        delegate int SumDelegate(int m, int n);
+
+        static void Task4()
+        {
+            SumDelegate sumDel = SumRange;
+
+            Console.WriteLine($"Сумма (1, 5): {sumDel(1, 5)}");
+            Console.WriteLine($"Сумма (10, 15): {sumDel(10, 15)}");
+            Console.WriteLine($"Сумма (100, 105): {sumDel(100, 105)}");
+        }
+
+        static void Main(string[] args)
+        {
+            Task4(); 
+
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();  
+        }
+    }
+}
+
+
